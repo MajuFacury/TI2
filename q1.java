24bf0b4b468db85fb172cdbd7dcdf4361edac06f
Maria Júlia Tavares Facury <85713325+MajuFacury@users.noreply.github.com>
2022-08-24 22:19:12 -0300

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,14 @@
+public class q1 {
+    public static void main(String[] args) {
+        int somar = soma(100,47);
+        System.out.println(somar);
+
+    }
+
+    public static int soma(int n1, int n2){
+        return n1+n2;
+
+    }
+
+   
+}
